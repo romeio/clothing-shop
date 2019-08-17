6b60478d43c7d8059826a0a7b64a59bf78c2780c
ram <theoutlier1@gmail.com>
2019-08-17 14:24:05 +0200

    created project with create react app and added files for home pages

---
+++ src/homepage.component.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+// FUNCTIONAL COMPONENT
+const HomePage =()=>(
+<div className='homepage'>
+<div className='directory-menu'>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>SHOP NOW</span>
+
+</div>
+
+
+</div>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>Jackets</span>
+
+</div>
+
+
+</div>
+
+
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+
+
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+<div className='menu-item'>
+<div className='content'>
+ <h1 className='title'>Hats</h1>
+ <span className='subtitle'>MEN</span>
+
+</div>
+</div>
+</div>
+</div>
+)
+export default HomePage;
